a71afa0be6372fc1a01a8cc94c742cf17e8af7c0
Reuven Mizrahi <rmizrahi45@gmail.com>
2025-03-26 08:00:01 -0400

    > updated app.test.js to include 'beforeAll' and 'afterAll' blocks since test did not complete as expected during the last run.

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,6 +1,16 @@
 const request = require("supertest");
 const app = require("../src/app");
 
+let server;
+
+beforeAll(() => {
+    server = app.listen(4000); // Start test server on a different port
+});
+
+afterAll((done) => {
+    server.close(done); // Close server after tests
+});
+
 describe("GET /", () => { 
     it("should return Hello, World!", async () => {
         const res = await request(app).get("/");
